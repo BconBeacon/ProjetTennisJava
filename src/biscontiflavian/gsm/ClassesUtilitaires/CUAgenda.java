af100f47ff2a0c836f9a6c4292aea5c3cbbd1e92
BiscontiFlavian <bisconti.flavian@condorcet.be>
2019-11-13 16:39:17 +0100

    Tentative de création de l'agenda
    Essai à la mise en place d'un agenda pour l'affectation de la date du
    déroulement de chaque match.

--- src/biscontiflavian/gsm/ClassesUtilitaires/CUAgenda.java
+++ src/biscontiflavian/gsm/ClassesUtilitaires/CUAgenda.java
@@ -1,12 +1,40 @@
 package biscontiflavian.gsm.ClassesUtilitaires;
 
+import java.time.LocalDate;
+
 public class CUAgenda {
-	//private ListLinked<CUDate> agenda;
 	private CUAgenda instance;
+	private CUDate[][] agenda;
 	
 	private CUAgenda()
 	{
+		LocalDate date = LocalDate.now().plusDays(1);
+		//heures[heure][minute]
+		int[][] heures = new int[7][];
+		for(int i=0;i<heures.length;i++)
+		{
+			heures[i] = new int[2];
+		}
 		
+		agenda = new CUDate[5][];
+		//Matchs de type 'simple homme'==========
+		//Jour 1 à Jour 4
+		agenda[0][0] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 1);
+		agenda[0][1] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 2);
+		agenda[0][2] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 3);
+		agenda[0][3] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 4);
+		agenda[0][4] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 5);
+		agenda[0][5] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 6);
+		agenda[0][6] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 7);
+		agenda[0][7] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 8, 0 , CUTypeMatch.SH, 8);
+		agenda[0][8] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 9);
+		agenda[0][9] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 10);
+		agenda[0][10] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 11);
+		agenda[0][11] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 12);
+		agenda[0][12] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 13);
+		agenda[0][13] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 14);
+		agenda[0][14] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 15);
+		agenda[0][15] = new CUDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth(), 10, 0 , CUTypeMatch.SH, 16);
 	}
 	
 	public CUAgenda getInstance()
--- src/biscontiflavian/gsm/ClassesUtilitaires/CUDate.java
+++ src/biscontiflavian/gsm/ClassesUtilitaires/CUDate.java
@@ -6,13 +6,17 @@ public class CUDate {
 	private int annee;
 	private int heure;
 	private int minute;
+	private CUTypeMatch typeMatch;
+	private int numeroMatch;
 	
-	public CUDate(int j, int m, int a, int h, int min)
+	public CUDate(int j, int m, int a, int h, int min, CUTypeMatch tm, int nm)
 	{
 		annee = a;
 		mois = m;
 		jour = j;
 		heure = h;
 		minute = min;
+		typeMatch = tm;
+		numeroMatch = nm;
 	}
 }
