361194c37fe6a65cece3ae864c6913ec5dc9502b
BiscontiFlavian <bisconti.flavian@condorcet.be>
2019-11-16 17:57:29 +0100

    Création des classes DAO
    Mise en place de la base des classes DAO et de la classe de connexion à
    la base de données.

---
+++ src/biscontiflavian/gsm/ClassesDAO/ConnexionDAO.java
@@ -0,0 +1,37 @@
+package biscontiflavian.gsm.ClassesDAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class ConnexionDAO {
+private static Connection instance = null;
+	
+	private ConnexionDAO(){
+		try{
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			String url = "jdbc:ucanaccess://./Ecole.accdb";
+			instance = DriverManager.getConnection(url);
+		}
+		catch(ClassNotFoundException ex){
+	JOptionPane.showMessageDialog(null, "Classe de driver introuvable" + ex.getMessage());
+			System.exit(0);
+		}
+		catch (SQLException ex) {
+	JOptionPane.showMessageDialog(null, "Erreur JDBC : " + ex.getMessage());
+		}
+		if (instance == null) {
+            JOptionPane.showMessageDialog(null, "La base de données est inaccessible, fermeture du programme.");
+            System.exit(0);
+        }
+	}
+	
+	public static Connection getInstance() {
+		if(instance == null){
+			new ConnexionDAO();
+		}
+		return instance;
+	}
+}
